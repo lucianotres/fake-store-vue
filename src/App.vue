93744736014a3866d253c6450c2c1ab032267581
Luciano Tres <lucianotres@outlook.com>
2025-11-20 19:03:01 -0300

    Layout inicial do app

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,107 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="page">
+    <div class="sidebar">
+      <!-- <nav-menu /> -->
+      <h6>Nav-menu aqui</h6>
+    </div>
+
+    <main>
+      <div class="top-row px-4 auth">
+        <h6>UsuarioAutenticado</h6>
+        <h6>escolha-cotacao</h6>
+      </div>
+
+      <article class="content px-4">
+        <RouterView />
+      </article>
+    </main>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.page {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+}
+
+main {
+  flex: 1;
+}
+
+.sidebar {
+  background-image: linear-gradient(180deg, rgb(5, 39, 103) 0%, #3a0647 70%);
+}
+
+.top-row {
+  background-color: #f7f7f7;
+  border-bottom: 1px solid #d6d5d5;
+  justify-content: flex-end;
+  height: 3.5rem;
+  display: flex;
+  align-items: center;
+}
+
+.top-row a,
+.top-row .btn-link {
+  white-space: nowrap;
+  margin-left: 1.5rem;
+  text-decoration: none;
+}
+
+.top-row a:hover,
+.top-row .btn-link:hover {
+  text-decoration: underline;
+}
+
+.top-row a:first-child {
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+@media (max-width: 640.98px) {
+  .top-row {
+    justify-content: space-between;
+  }
+
+  .top-row a,
+  .top-row .btn-link {
+    margin-left: 0;
+  }
+}
+
+@media (min-width: 641px) {
+  .page {
+    flex-direction: row;
+  }
+
+  .sidebar {
+    width: 250px;
+    height: 100vh;
+    position: sticky;
+    top: 0;
+  }
+
+  .top-row {
+    position: sticky;
+    top: 0;
+    z-index: 1;
+  }
+
+  .top-row.auth a:first-child {
+    flex: 1;
+    text-align: right;
+    width: 0;
+  }
+
+  .top-row,
+  article {
+    padding-left: 2rem !important;
+    padding-right: 1.5rem !important;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>Loja Fake</h1>
+  <p>Bem vindos a nossa loja fake!</p>
+
+  <p>Escolha uma opção no menu para iniciar</p>
+
+  <h6>Colocar cotação aqui</h6>
+</template>
