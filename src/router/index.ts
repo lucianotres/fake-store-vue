cb315e022c1f492df05d8250b6c92686e68c85ac
Luciano Tres <lucianotres@outlook.com>
2025-11-20 19:46:57 -0300

    Adicionado rota coringa para não encontrado

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
+import NotFound from '@/views/NotFound.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,7 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
+    { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
   ],
 })
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="not-found">
+    <h1>404 - Página não encontrada</h1>
+    <RouterLink to="/">Voltar para a Home</RouterLink>
+  </div>
+</template>
